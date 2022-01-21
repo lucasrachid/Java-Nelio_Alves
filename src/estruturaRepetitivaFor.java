bf2ca8d4f813bcc62ae6cad31127fd3c240feca8
lucasrachid <lucasrachid@hotmail.com>
2022-01-21 10:51:13 -0300

    Exercicios For

---
+++ src/estruturaRepetitivaFor.java
@@ -0,0 +1,55 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class estruturaRepetitivaFor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner ed = new Scanner(System.in);
+
+        //Exercício Estrutura Repetitiva (For) - 01 - PDF
+//        System.out.printf("Diga um número para X: ");
+//        int x = ed.nextInt();
+//        for (int number = 0; number <= x; number++ ) {
+//            if (number % 2 != 0) {
+//                System.out.println(number);
+//            }
+//        }
+
+        //Exercício Estrutura Repetitiva (For) - 02 - PDF
+//        int in = 0, out = 0;
+//        System.out.printf("Diga um valor para X: ");
+//        int x = ed.nextInt();
+//        for(int y = 0; y < x; y++) {
+//            System.out.printf("Diga um valor: ");
+//            int valor = ed.nextInt();
+//            if (valor >= 10 && valor <= 20) {
+//                in += 1;
+//            } else {
+//                out += 1;
+//            }
+//        }
+//        System.out.printf("%d in%n%d out", in, out);
+
+        //Exercício Estrutura Repetitiva (For) - 03 - PDF
+//        System.out.printf("Quantas vezes deseja realizar o teste ? ");
+//        int n = ed.nextInt();
+//        for (int i=0; i<n; i++) {
+//            System.out.printf("Diga um valor para A: ");
+//            double a = ed.nextDouble();
+//            System.out.printf("Diga um valor para B: ");
+//            double b = ed.nextDouble();
+//            System.out.printf("Diga um valor para C: ");
+//            double c = ed.nextDouble();
+//
+//            double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+//
+//            System.out.printf("%.1f%n", media);
+//        }
+
+        //Exercício Estrutura Repetitiva (For) - 03 - PDF
+
+
+
+
+    }
+}
